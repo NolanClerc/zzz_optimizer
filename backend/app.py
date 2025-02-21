ff0fc80ac9844cd165411995ef3583b3c8afe190
NolanClerc <clercnolan@orange.fr>
2025-02-21 15:06:25 +0100

    back2

--- backend/app.py
+++ backend/app.py
@@ -88,4 +88,5 @@ def get_all_characters():
     return jsonify(all_characters)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT environment variable
+    app.run(host='0.0.0.0', port=port, debug=True)
